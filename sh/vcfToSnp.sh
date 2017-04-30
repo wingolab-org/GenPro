747bbd595ec5f19f33e23c877aa379186888576d
Thomas Wingo <thomas.wingo@emory.edu>
2017-04-30 11:19:44 -0400

    minor edit

--- sh/vcfToSnp.sh
+++ sh/vcfToSnp.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
 
-if [ $# != 2 ]; then
-  echo "$0 <vcf file> <sample list>"
+if [ $# != 3 ]; then
+  echo "$0 <vcf file> <sample list> <output prefix>"
   exit
 fi
 
-bcftools query -H -f '%CHROM\t%POS\t%REF\t%ALT\t%TYPE[\t%IUPACGT]\n' -S ${2} ${1} | vcfToSnp > ${1}.snp
+bcftools query -H -f '%CHROM\t%POS\t%REF\t%ALT\t%TYPE[\t%IUPACGT]\n' -S ${2} ${1} | vcfToSnp > ${3}.snp
